f8264ecc5824f4bcbc54404c3748d988a4b309b0
Rachel <1623404291@qq.com>
2019-07-19 18:42:11 +0800

    增加画布新分支

--- app/src/main/java/com/test/hsw/MainActivity.java
+++ app/src/main/java/com/test/hsw/MainActivity.java
@@ -1,13 +1,51 @@
 package com.test.hsw;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * https://www.cnblogs.com/liangstudyhome/p/4143498.html
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new SampleView(this));
+    }
+
+    private static class SampleView extends View {
+        private static final int LAYER_FLAGS = Canvas.ALL_SAVE_FLAG;
+
+        private Paint mPaint;
+        private ImageView imageView;
+
+        public SampleView(Context context) {
+            super(context);
+            setFocusable(true);
+            imageView = new ImageView(context);
+            imageView.setId(R.id.img);
+
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.WHITE);
+            canvas.translate(10, 10);
+            mPaint.setColor(Color.RED);
+            canvas.drawCircle(75, 75, 75, mPaint);
+            canvas.saveLayerAlpha(0, 0, 200, 200, 0x88, LAYER_FLAGS);
+            mPaint.setColor(Color.BLUE);
+            canvas.drawCircle(125, 125, 75, mPaint);
+            canvas.restore();
+        }
     }
 }
